a9b0c9cdc4f3b263f09f43c492869eb824e7f366
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2024-01-10 15:38:45 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.kullanicitxt = new System.Windows.Forms.TextBox();
+            this.epostatxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.parolatxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rolcmb = new System.Windows.Forms.ComboBox();
+            this.kaydetbtn = new System.Windows.Forms.Button();
+            this.kaydetEbtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listelebtn = new System.Windows.Forms.Button();
+            this.listeleEbtn = new System.Windows.Forms.Button();
+            this.silbtn = new System.Windows.Forms.Button();
+            this.silebtn = new System.Windows.Forms.Button();
+            this.guncellebtn = new System.Windows.Forms.Button();
+            this.guncelleebtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // kullanicitxt
+            // 
+            this.kullanicitxt.Location = new System.Drawing.Point(119, 39);
+            this.kullanicitxt.Name = "kullanicitxt";
+            this.kullanicitxt.Size = new System.Drawing.Size(161, 22);
+            this.kullanicitxt.TabIndex = 1;
+            // 
+            // epostatxt
+            // 
+            this.epostatxt.Location = new System.Drawing.Point(119, 70);
+            this.epostatxt.Name = "epostatxt";
+            this.epostatxt.Size = new System.Drawing.Size(161, 22);
+            this.epostatxt.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Eposta";
+            // 
+            // parolatxt
+            // 
+            this.parolatxt.Location = new System.Drawing.Point(119, 98);
+            this.parolatxt.Name = "parolatxt";
+            this.parolatxt.Size = new System.Drawing.Size(161, 22);
+            this.parolatxt.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Parola";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Rol";
+            // 
+            // rolcmb
+            // 
+            this.rolcmb.FormattingEnabled = true;
+            this.rolcmb.Items.AddRange(new object[] {
+            "Admin",
+            "User"});
+            this.rolcmb.Location = new System.Drawing.Point(119, 126);
+            this.rolcmb.Name = "rolcmb";
+            this.rolcmb.Size = new System.Drawing.Size(121, 24);
+            this.rolcmb.TabIndex = 7;
+            // 
+            // kaydetbtn
+            // 
+            this.kaydetbtn.Location = new System.Drawing.Point(305, 39);
+            this.kaydetbtn.Name = "kaydetbtn";
+            this.kaydetbtn.Size = new System.Drawing.Size(89, 23);
+            this.kaydetbtn.TabIndex = 8;
+            this.kaydetbtn.Text = "Kaydet";
+            this.kaydetbtn.UseVisualStyleBackColor = true;
+            this.kaydetbtn.Click += new System.EventHandler(this.kaydetbtn_Click);
+            // 
+            // kaydetEbtn
+            // 
+            this.kaydetEbtn.Location = new System.Drawing.Point(305, 70);
+            this.kaydetEbtn.Name = "kaydetEbtn";
+            this.kaydetEbtn.Size = new System.Drawing.Size(89, 23);
+            this.kaydetEbtn.TabIndex = 9;
+            this.kaydetEbtn.Text = "Kaydet-E";
+            this.kaydetEbtn.UseVisualStyleBackColor = true;
+            this.kaydetEbtn.Click += new System.EventHandler(this.kaydetEbtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 170);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(452, 189);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // listelebtn
+            // 
+            this.listelebtn.Location = new System.Drawing.Point(305, 98);
+            this.listelebtn.Name = "listelebtn";
+            this.listelebtn.Size = new System.Drawing.Size(89, 23);
+            this.listelebtn.TabIndex = 11;
+            this.listelebtn.Text = "Listele";
+            this.listelebtn.UseVisualStyleBackColor = true;
+            this.listelebtn.Click += new System.EventHandler(this.listelebtn_Click);
+            // 
+            // listeleEbtn
+            // 
+            this.listeleEbtn.Location = new System.Drawing.Point(305, 126);
+            this.listeleEbtn.Name = "listeleEbtn";
+            this.listeleEbtn.Size = new System.Drawing.Size(89, 23);
+            this.listeleEbtn.TabIndex = 12;
+            this.listeleEbtn.Text = "Listele E";
+            this.listeleEbtn.UseVisualStyleBackColor = true;
+            this.listeleEbtn.Click += new System.EventHandler(this.listeleEbtn_Click);
+            // 
+            // silbtn
+            // 
+            this.silbtn.Location = new System.Drawing.Point(400, 39);
+            this.silbtn.Name = "silbtn";
+            this.silbtn.Size = new System.Drawing.Size(89, 23);
+            this.silbtn.TabIndex = 13;
+            this.silbtn.Text = "Sil";
+            this.silbtn.UseVisualStyleBackColor = true;
+            this.silbtn.Click += new System.EventHandler(this.silbtn_Click);
+            // 
+            // silebtn
+            // 
+            this.silebtn.Location = new System.Drawing.Point(400, 70);
+            this.silebtn.Name = "silebtn";
+            this.silebtn.Size = new System.Drawing.Size(89, 23);
+            this.silebtn.TabIndex = 14;
+            this.silebtn.Text = "Sil-E";
+            this.silebtn.UseVisualStyleBackColor = true;
+            this.silebtn.Click += new System.EventHandler(this.silebtn_Click);
+            // 
+            // guncellebtn
+            // 
+            this.guncellebtn.Location = new System.Drawing.Point(400, 98);
+            this.guncellebtn.Name = "guncellebtn";
+            this.guncellebtn.Size = new System.Drawing.Size(89, 23);
+            this.guncellebtn.TabIndex = 15;
+            this.guncellebtn.Text = "Güncelle";
+            this.guncellebtn.UseVisualStyleBackColor = true;
+            this.guncellebtn.Click += new System.EventHandler(this.guncellebtn_Click);
+            // 
+            // guncelleebtn
+            // 
+            this.guncelleebtn.Location = new System.Drawing.Point(400, 126);
+            this.guncelleebtn.Name = "guncelleebtn";
+            this.guncelleebtn.Size = new System.Drawing.Size(89, 23);
+            this.guncelleebtn.TabIndex = 16;
+            this.guncelleebtn.Text = "Güncelle-E";
+            this.guncelleebtn.UseVisualStyleBackColor = true;
+            this.guncelleebtn.Click += new System.EventHandler(this.guncelleebtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 389);
+            this.Controls.Add(this.guncelleebtn);
+            this.Controls.Add(this.guncellebtn);
+            this.Controls.Add(this.silebtn);
+            this.Controls.Add(this.silbtn);
+            this.Controls.Add(this.listeleEbtn);
+            this.Controls.Add(this.listelebtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.kaydetEbtn);
+            this.Controls.Add(this.kaydetbtn);
+            this.Controls.Add(this.rolcmb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.parolatxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.epostatxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.kullanicitxt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox kullanicitxt;
+        private System.Windows.Forms.TextBox epostatxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox parolatxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox rolcmb;
+        private System.Windows.Forms.Button kaydetbtn;
+        private System.Windows.Forms.Button kaydetEbtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button listelebtn;
+        private System.Windows.Forms.Button listeleEbtn;
+        private System.Windows.Forms.Button silbtn;
+        private System.Windows.Forms.Button silebtn;
+        private System.Windows.Forms.Button guncellebtn;
+        private System.Windows.Forms.Button guncelleebtn;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp3
+{
+    
+    public partial class Form1 : Form
+    {
+        HesaplarEntities he = new HesaplarEntities();
+        int id;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void kaydetbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = "Server=localhost\\sqlexpress;Database=Hesaplar;Trusted_Connection=True;";
+                conn.Open();
+                String sql = "insert into kullanici(kullaniciadi,eposta,parola,rol) values('" + kullanicitxt.Text + "','" + epostatxt.Text + "','" + parolatxt.Text + "','" + rolcmb.Text + "')";
+                SqlCommand scom = new SqlCommand();
+                scom.Connection = conn;
+                scom.CommandText = sql;
+                scom.ExecuteNonQuery();
+                MessageBox.Show("Kaydedildi");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }         
+        }
+
+        private void kaydetEbtn_Click(object sender, EventArgs e)
+        {
+            kullanici kullanici = new kullanici();
+            kullanici.kullaniciadi = kullanicitxt.Text;
+            kullanici.eposta = epostatxt.Text;
+            kullanici.parola = parolatxt.Text;
+            kullanici.rol = rolcmb.Text;
+            he.kullanici.Add(kullanici);       
+            he.SaveChanges();
+        }
+
+        private void listelebtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Server=localhost\\sqlexpress;Database=Hesaplar;Trusted_Connection=True;";
+            conn.Open();
+            DataTable dt=new DataTable();
+            String sql = "select * from kullanici";
+            SqlDataAdapter sda=new SqlDataAdapter(sql, conn);
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void listeleEbtn_Click(object sender, EventArgs e)
+        {
+            var sonuclar = he.kullanici.ToList();
+            dataGridView1.DataSource = sonuclar;
+        }
+
+        private void silbtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Server=localhost\\sqlexpress;Database=Hesaplar;Trusted_Connection=True;";
+            conn.Open();
+            String sql = "delete from kullanici where kullaniciadi='"+kullanicitxt.Text+"' or eposta='"+epostatxt.Text+"'";
+            SqlCommand scom = new SqlCommand();
+            scom.Connection = conn;
+            scom.CommandText = sql;
+            int sonuc=scom.ExecuteNonQuery();
+            if (sonuc > 0)
+            {
+                MessageBox.Show("Kayıt Silindi");
+            }
+            else
+            {
+                MessageBox.Show("Kayıt bulunamadı");
+            }
+        }
+
+        private void silebtn_Click(object sender, EventArgs e)
+        {
+            var kayit = he.kullanici.Where(a => a.kullaniciadi == kullanicitxt.Text || a.eposta == epostatxt.Text).FirstOrDefault();
+            he.kullanici.Remove(kayit);
+            he.SaveChanges();
+        }
+
+        private void guncellebtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Server=localhost\\sqlexpress;Database=Hesaplar;Trusted_Connection=True;";
+            conn.Open();
+            String sql = "update kullanici set kullaniciadi='"+kullanicitxt.Text+"',eposta='"+epostatxt.Text+"',parola='"+parolatxt.Text+"',rol='"+rolcmb.Text+"' where id="+id;
+            SqlCommand scom = new SqlCommand();
+            scom.Connection = conn;
+            scom.CommandText = sql;
+            scom.ExecuteNonQuery();
+            MessageBox.Show("Güncellendi");
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            id = Convert.ToInt32(dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[0].Value);
+            kullanicitxt.Text = dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[1].Value.ToString();
+            epostatxt.Text = dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[2].Value.ToString();
+            parolatxt.Text = dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[3].Value.ToString();
+            rolcmb.Text = dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[4].Value.ToString();
+
+        }
+
+        private void guncelleebtn_Click(object sender, EventArgs e)
+        {
+            var kayit = he.kullanici.Where(a => a.id==id).FirstOrDefault();
+            kayit.kullaniciadi = kullanicitxt.Text;
+            kayit.eposta=epostatxt.Text;
+            kayit.parola = parolatxt.Text;
+            kayit.rol=rolcmb.Text;  
+            he.Entry(kayit).State=System.Data.Entity.EntityState.Modified;
+            he.SaveChanges();
+        }
+    }
+}
